d3c7ba742334a739f416835d4a77b1d686f9bd45
TJ <tjpataria@TJs-MacBook-Pro.local>
2020-10-12 15:32:59 +0100

    homework

---
+++ homework/todo_list/src/App.vue
@@ -0,0 +1,109 @@
+<template>
+  <div id="app">
+    <h1>ToDo's</h1>
+    <form v-on:submit.prevent="saveNewItem">
+      <label for="new-item">Add a new item:</label>
+      <input type="text" id="new-item" v-model="newItem">
+      <label for="radio" v-on:click="isHighPriority(index)">High</label>
+      <input type="radio" name="radio-button" id="radio-high">
+      <label for="radio" v-on:click="!isHighPriority(index)">Low</label>
+      <input type="radio" name="radio-button" id="radio-low">
+      <input type="submit" value="Save New Item">
+    </form>
+
+    <ul>
+      <li v-for="(item, index) in items" v-bind:class="item.isHighPriority ? 'high-priority' : 'not-priority'" :key="index">
+        <span>{{ item.name }}</span>
+        </li> 
+    </ul>  
+
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      items: [
+        {name: 'Mop the floor', isHighPriority: false},
+        {name: 'Wash the clothes', isHighPriority: true},
+        {name: 'Make dinner', isHighPriority: false},
+      ],
+      newItem: "",
+    };
+  },
+  methods: {
+    saveNewItem: function () {
+        this.items.push({
+          name: this.newItem, 
+          isHighPriority: false,
+        });
+        this.newItem = "";
+      },
+      doneItem: function (index) {
+        this.items[index].isHighPriority = true;
+      },
+      deleteItem: function (index) {
+        this.items.splice(index, 1);
+      },
+    },
+  };
+</script>
+
+<style>
+#app {
+  width: 60%;
+  margin: 0 auto;
+  font-family: 'Lato', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+li {
+  margin: 20px 0;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+
+li span {
+  padding: 8px;
+}
+
+li button {
+  background: #f2360c;
+  color: #fff;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+}
+
+li.not-priority {
+  border: 2px solid #1a681e;
+  color: #1a681e;
+}
+
+li.high-priority {
+  border: 2px solid #f2360c;
+}
+
+input[type="text"] {
+  padding: 10px;
+  width: 50%;
+  margin:5px;
+}
+
+button, input[type="submit"]{
+  padding: 10px;
+  background: #000;
+  color: #fff;
+  cursor: pointer;
+  border: 1px solid #000;
+}
+</style>
